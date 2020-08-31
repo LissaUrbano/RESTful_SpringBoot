d85497f715c6874533b8bf11cd883462ae65fa4a
Lissa Urbano <lissaurbano@gmail.com>
2020-08-31 00:47:36 -0300

    first commit

---
+++ aula_25-08-2020/curso_restful_springboot/src/main/java/com/example/curso_restful_springboot/CursoRestfulSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.example.curso_restful_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoRestfulSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoRestfulSpringbootApplication.class, args);
+	}
+}
---
+++ aula_25-08-2020/curso_restful_springboot/src/test/java/com/example/curso_restful_springboot/CursoRestfulSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curso_restful_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoRestfulSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
