f229a381590226e298bc8165d9146fa289fe85a5
Lissa Urbano <lissaurbano@gmail.com>
2020-09-01 22:37:41 -0300

    #feature - GetMapping clientes id

---
+++ aula_25-08-2020/curso_restful_springboot/src/main/java/com/example/curso_restful_springboot/controller/Cliente.java
@@ -0,0 +1,8 @@
+package com.example.curso_restful_springboot.controller;
+
+public class Cliente {
+    public int id;
+    public String nome;
+    public String endereco;
+    public double saldo;
+}
--- aula_25-08-2020/curso_restful_springboot/src/main/java/com/example/curso_restful_springboot/controller/ClienteController.java
+++ aula_25-08-2020/curso_restful_springboot/src/main/java/com/example/curso_restful_springboot/controller/ClienteController.java
@@ -1,5 +1,10 @@
 package com.example.curso_restful_springboot.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,14 +12,49 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClienteController {
     
-    @GetMapping("/cliente")
-    public String getClientes(){
-        return "Vai retornar um dia todos os clientes da BD";
+    List<Cliente> clientes;
+
+    @PostConstruct
+    public void init(){
+
+        Cliente c1 = new Cliente();
+        c1.id = 1;
+        c1.nome = "jose";
+        c1.endereco = "Rua X, 99";
+        c1.saldo = 100;
+
+        Cliente c2 = new Cliente();
+        c2.id = 2;
+        c2.nome = "Maria";
+        c2.endereco = "Rua Y, 24";
+        c2.saldo = 200;
+
+        Cliente c3 = new Cliente();
+        c3.id = 3;
+        c3.nome = "Fernanda";
+        c3.endereco = "Rua W, 57";
+        c3.saldo = 300;
+
+        clientes = Arrays.asList(c1, c2, c3);
+
+    }
+    
+    @GetMapping("/clientes")
+    public List<Cliente> getClientes(){
+        return clientes;
     }
 
-    @GetMapping("/cliente/{codigo}")
-    public String getCliente(@PathVariable int codigo){
-        return "Vai retornar um dia o cliente de codigo" + codigo;
+    @GetMapping("/clientes/{id}")
+    public Cliente getCliente(@PathVariable int id){
+        Cliente cliente = null;
+
+        for(Cliente aux : clientes){
+            if(aux.id == id){
+                cliente = aux;
+                break;
+            }
+        }
+        return cliente;
     }
 
 
